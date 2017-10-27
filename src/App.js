5d26cc6984a35b13d43373ec5530b8ce680a2807
Arnaz <arnazmursid@gmail.com>
2017-10-27 13:17:00 +0700

    add div

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import './App.css';
+import '../media/css/App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <div className="App-anim"></div>
+          <div className="App-anim2"></div>
+          <h1 className="App-h1">HI! I'M ARNAZ</h1>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="App-introduce">
+
+        </div>
       </div>
     );
   }
