aba9dca4f6c1a3ba364c92dc278e4721cc33a427
Arnaz <arnazmursid@gmail.com>
2017-11-06 21:38:46 +0700

    Change render method into Redirect

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import Redirect from './Redirect';
 import './index.css';
 
 ReactDOM.render(
-  <App />,
+  <Redirect />,
   document.getElementById('root')
 );
