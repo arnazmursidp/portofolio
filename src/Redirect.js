5b03e79473f1d682aac84a9da31f0f860457234c
Arnaz <arnazmursid@gmail.com>
2017-11-06 21:40:45 +0700

    add redirect component

---
+++ src/Redirect.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import '../media/css/Redirect.css';
+
+class Redirect extends Component{
+    render(){
+        return(
+            <h1>Use desktop for better experience</h1>
+        )
+    }
+}
+
+export default Redirect
