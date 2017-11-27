91f9c7b9d42df66efb9943e8c45cf2d9dfc1007d
Arnaz <arnazmursid@gmail.com>
2017-11-27 15:12:47 +0700

    add handle scroll function

--- src/App.js
+++ src/App.js
@@ -2,14 +2,30 @@ import React, { Component } from 'react';
 import Introduce from './Introduce';
 import Header from './Header';
 class App extends Component {
-  // constructor(props){
-  //   super(p)
-  // }
+  constructor(props){
+    super();
+    this.state = {
+      startAnimation: false
+    }
+  }
+
+  handleScroll = () => {
+    if(document.documentElement.scrollTop > 650){
+      this.setState({
+        startAnimation: true
+      })
+    }
+  }
+
+  componentDidMount() {
+    window.onscroll = () => this.handleScroll()
+  }
+
   render() {
     return (
       <div>
         <Header/>
-        <Introduce/>
+        <Introduce startAnimation={this.state.startAnimation}/>
       </div>  
     );
   }
