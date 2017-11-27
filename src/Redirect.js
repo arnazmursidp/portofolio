a4039708659f58801fb4d5d86cab311fa8257456
Arnaz <arnazmursid@gmail.com>
2017-11-27 10:37:47 +0700

    remove router

--- src/Redirect.js
+++ src/Redirect.js
@@ -1,10 +1,20 @@
 import React, { Component } from 'react';
 import '../media/css/Redirect.css';
+import {
+    BrowserRouter as Router,
+    Route,
+    Link
+  } from 'react-router-dom';
+import App from './App';
 
 class Redirect extends Component{
     render(){
         return(
-            <h1>Use desktop for better experience</h1>
+            <Router>    
+                <Link to="/home">Use desktop for better experience</Link>
+                
+                <Route path="/home" component={App}/>
+            </Router>
         )
     }
 }
