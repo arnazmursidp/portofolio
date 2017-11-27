1b50e134039e1773d98214ac2e95068c30c8f7f2
Arnaz <arnazmursid@gmail.com>
2017-11-27 12:17:45 +0700

    simplify code in app.js

--- src/App.js
+++ src/App.js
@@ -1,39 +1,16 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import '../media/css/App.css';
-import { Col, Row } from 'react-bootstrap';
-
-
+import Introduce from './Introduce';
+import Header from './Header';
 class App extends Component {
   // constructor(props){
   //   super(p)
   // }
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <div className="App-anim"></div>
-          <div className="App-anim2"></div>
-          <div className="App-anim3"></div>
-          <div className="divh1">
-            <h1 className="App-h1">HI! I'M ARNAZ</h1>
-          </div>
-        </div>
-        <div className="App-introduce">
-          <Row>
-            <Col xs={3} md={3}>
-              <div className="hiddens">
-                
-              </div>
-            </Col>
-            <Col xs={6} md={6}>
-              <div className="desc">
-                
-              </div>
-            </Col>
-          </Row>
-        </div>
-      </div>
+      <div>
+        <Header/>
+        <Introduce/>
+      </div>  
     );
   }
 }
