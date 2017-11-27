87c6fed361ba81ff5883efd0a9f493065c0319cf
Arnaz <arnazmursid@gmail.com>
2017-11-27 17:26:40 +0700

    add carousel js

---
+++ src/Carousel.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import '../media/css/Header.css';
+import { Carousel } from 'react-bootstrap';
+class Header extends Component {
+  // constructor(props){
+  //   super(p)
+  // }
+  render() {
+    return (
+      <div className="App-carousel">
+        
+      </div>
+    );
+  }
+}
+
+export default Header;
