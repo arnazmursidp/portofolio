6d2834bd2f6edd4d267ea09e5b0fc46feb78f11b
Arnaz <arnazmursid@gmail.com>
2017-11-27 19:50:43 +0700

    add contact component

---
+++ src/Contact.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import '../media/css/Contact.css';
+class Contact extends Component {
+  render() {
+    return (
+      <div className="App-contact">
+      </div>
+    );
+  }
+}
+
+export default Contact;
