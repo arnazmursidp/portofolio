d56f52f5ae731e66e92d83ae137b10c8406f61a0
Arnaz <arnazmursid@gmail.com>
2017-11-27 12:17:55 +0700

    add header component

---
+++ src/Header.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import '../media/css/App.css';
+import { Col, Row, Thumbnail, Collapse, Carousel } from 'react-bootstrap';
+class Header extends Component {
+  // constructor(props){
+  //   super(p)
+  // }
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <div className="App-anim"></div>
+          <div className="App-anim2"></div>
+          <div className="App-anim3"></div>
+          <div className="divh1">
+            <h1 className="App-h1">HI! I'M ARNAZ</h1>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Header;
