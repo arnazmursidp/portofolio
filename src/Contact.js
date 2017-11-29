aa3b97406c9bec4b359716a4da103365f9fb38db
Arnaz <arnazmursid@gmail.com>
2017-11-29 22:25:01 +0700

    add div for contact

--- src/Contact.js
+++ src/Contact.js
@@ -1,9 +1,20 @@
 import React, { Component } from 'react';
 import '../media/css/Contact.css';
+import { Jumbotron, Button } from 'react-bootstrap';
+
 class Contact extends Component {
   render() {
     return (
       <div className="App-contact">
+         <div className="jumboTron">
+            <div>
+              <h1>CONTACT</h1>
+            </div>
+            <div>
+              <h2>081905645803</h2>
+              <h4>Kp. Pasirhandap, Ds. Pagerwangi, Kab. Bandung Barat, Lembang, Indonesia</h4>
+            </div>  
+         </div>
       </div>
     );
   }
