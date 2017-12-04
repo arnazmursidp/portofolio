ba38c6f1eaf9818596928b23059c08d4ba68708f
Arnaz <arnazmursid@gmail.com>
2017-12-04 17:40:10 +0700

    remove unused components

--- src/Contact.js
+++ src/Contact.js
@@ -1,18 +1,16 @@
 import React, { Component } from 'react';
 import '../media/css/Contact.css';
-import { Jumbotron, Button } from 'react-bootstrap';
 
 class Contact extends Component {
   render() {
     return (
       <div className="App-contact">
          <div className="jumboTron">
-            <div>
-              <h1>CONTACT</h1>
+              <div className="contact">    
+                <h1>Contact</h1>
             </div>
             <div>
               <h2>081905645803</h2>
-              <h4>Kp. Pasirhandap, Ds. Pagerwangi, Kab. Bandung Barat, Lembang, Indonesia</h4>
             </div>  
          </div>
       </div>
