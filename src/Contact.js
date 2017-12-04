f92b61b08567a88ef2d49f490919d22c35b587e4
Arnaz <arnazmursid@gmail.com>
2017-12-04 22:14:50 +0700

    add animation condition for all animationed tag

--- src/Contact.js
+++ src/Contact.js
@@ -3,14 +3,15 @@ import '../media/css/Contact.css';
 
 class Contact extends Component {
   render() {
+    var start = this.props.startAnimationContact;
     return (
-      <div className="App-contact">
+      <div className={start === true ? "App-contact" : "App-contact-bg"}>
          <div className="jumboTron">
             <div>
               <h2>081905645803</h2>
             </div>  
-            <div className="contact">    
-                <h1>Contact</h1>
+            <div className={start === true ? "contact" : "hidden"}>    
+              <h1 className={start === true ? "h1-jumbotron" : "hidden"}>Contact</h1>
             </div>
          </div>
       </div>
