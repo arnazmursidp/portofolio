9cdb98861ef1f3183cdafc415814ccd2ec2bcd4c
Arnaz <arnazmursid@gmail.com>
2017-12-07 16:17:02 +0700

    add condition for  socmed animation

--- src/Contact.js
+++ src/Contact.js
@@ -7,7 +7,7 @@ class Contact extends Component {
     return (
       <div className={start === true ? "App-contact" : "App-contact-bg"}>
          <div className="jumboTron">
-            <div className="socialMedia">
+            <div className={start === true ? "socialMedia" : 'hidden'}>
               <div><h4><a href="https://instagram.com/arnazmursidp">Instagram</a></h4><i className="fa fa-instagram fa-lg"></i></div>
               <div><h4><a href="https://twitter.com/arnazmursidp">Twitter</a></h4><i className="fa fa-twitter fa-lg"></i></div>
               <div><h4><a href="https://facebook.com/arnazmursidp">Facebook</a></h4><i className="fa fa-facebook fa-lg"></i></div>
