f1be18b283824186cde602da904a82130ff22953
Arnaz <arnazmursid@gmail.com>
2018-02-19 17:43:46 +0700

    add line and description

--- src/Header.js
+++ src/Header.js
@@ -9,11 +9,12 @@ class Header extends Component {
     return (
       <div className="App">
         <div className="App-header">
-          <div className="App-anim"></div>
-          <div className="App-anim2"></div>
-          <div className="App-anim3"></div>
           <div className="divh1">
-            <h1 className="App-h1">HI! I'M ARNAZ</h1>
+            <h1 className="App-h1">Arnaz Mursid Prakasa</h1>
+            <div className="div-line"></div>
+            <h3>I'm based on Bandung, East Java, Indonesia, 
+              and I do front end web development using reactjs and mobile using React Native
+              I also have skills about CI and Laravel.</h3>
           </div>
         </div>
       </div>
