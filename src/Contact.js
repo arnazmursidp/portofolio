47fd859ada897a3c15246d1f2d9051017c1d6e75
Arnaz <arnazmursid@gmail.com>
2018-06-21 10:59:01 +0700

    fix icon and change socmed display

--- src/Contact.js
+++ src/Contact.js
@@ -7,11 +7,15 @@ class Contact extends Component {
     return (
       <div className={start === true ? "App-contact" : "App-contact-bg"}>
          <div className="jumboTron">
-            <div className={start === true ? "socialMedia" : 'hidden'}>
-              <div><h4><a href="https://instagram.com/arnazmursidp">Instagram</a></h4><i className="fa fa-instagram fa-lg"></i></div>
-              <div><h4><a href="https://twitter.com/arnazmursidp">Twitter</a></h4><i className="fa fa-twitter fa-lg"></i></div>
-              <div><h4><a href="https://facebook.com/arnazmursidp">Facebook</a></h4><i className="fa fa-facebook fa-lg"></i></div>
-              <div><h4 className="small">081905645803</h4><i className="fa fa-phone fa-lg"></i></div>
+            <div className={start === true ? "socialMedia" : 'nones'}>
+              <div className="divRow">
+                <div className={start === true ? "lefts" : 'hidden'} style={{ backgroundColor: 'rgb(250, 250, 250)'}}><a href="https://instagram.com/arnazmursidp"><i className="fa fa-instagram fa-lg"></i></a></div>
+                <div className={start === true ? "rights" : 'hidden'} style={{ backgroundColor: 'rgb(247, 247, 247)'}}><a href="https://twitter.com/arnazmursidp"><i className="fa fa-twitter fa-lg"></i></a></div>
+              </div>
+              <div className="divRow">
+                <div className={start === true ? "lefts" : 'hidden'} style={{ backgroundColor: 'rgb(243, 243, 243)'}}><a href="https://facebook.com/arnazmursidp"><i className="fa fa-facebook fa-lg"></i></a></div>
+                <div className={start === true ? "rights" : 'hidden'}><i className="fa fa-phone fa-lg"></i></div>
+              </div>
             </div>
          </div>
       </div>
