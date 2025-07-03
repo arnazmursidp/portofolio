89f20d45179d35c4b0b1755c0f286a802babcdc9
Arnaz <arnazmursid@gmail.com>
2025-07-03 17:31:39 +0700

    feat(grid): adjust grid height

--- src/components/Works/index.tsx
+++ src/components/Works/index.tsx
@@ -2,7 +2,7 @@ import "./index.scss";
 import WorksSection from "./WorksSection";
 import { getWorkList } from "../../utils/api";
 import { useEffect, useState } from "react";
-import { Grid, Row, Col } from 'react-flexbox-grid';
+import { Grid, Row, Col } from "react-flexbox-grid";
 
 const Index = () => {
   const [workList, setWorkList] = useState<Array<any>>([]);
@@ -18,18 +18,21 @@ const Index = () => {
     <div data-scroll-section className="works-container">
       <div className="works-section-container">
         <div style={{ width: "50vw" }}>
-          <Row>
-            {workList?.map(({ title, description, year, stacks, bgColor }) => (
-              <WorksSection
-                key={title}
-                title={title}
-                description={description}
-                stacks={stacks}
-                year={year}
-                bgColor={bgColor}
-              />
-            ))}
-          </Row>
+          <Grid fluid>
+            <Row>
+              {workList?.map(({ title, description, year, stacks, bgColor }) => (
+                <Col lg={4} xs={12} md={6} key={title}>
+                  <WorksSection
+                    title={title}
+                    description={description}
+                    stacks={stacks}
+                    year={year}
+                    bgColor={bgColor}
+                  />
+                </Col>
+              ))}
+            </Row>
+          </Grid>
         </div>
       </div>
     </div>
